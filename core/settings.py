ffd0ba33decdd59f6bc9a5d9ac05ff1b188b1441
sayfullo077 <new81305@gmail.com>
2024-09-21 21:22:01 +0500

    fixed 1

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import json
+
 import os, dj_database_url
 from pathlib import Path
 from dotenv import load_dotenv, dotenv_values
@@ -30,7 +30,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv("DEBUG"))
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+
 
 # Application definition
 
