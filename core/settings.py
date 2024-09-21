2a7c744f8841401948049846a499b614ef4c52f6
sayfullo077 <new81305@gmail.com>
2024-09-21 16:45:04 +0500

    Change for Heroku

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(config["DEBUG"])
 
-ALLOWED_HOSTS = config["ALLOWED_HOSTS"].split(",")
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
