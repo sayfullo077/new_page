0e47d92356efb0648eb0f33166b308e9b545e161
sayfullo077 <new81305@gmail.com>
2024-09-21 19:04:54 +0500

    first commitx

--- core/settings.py
+++ core/settings.py
@@ -150,5 +150,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config["EMAIL_USER"]
-EMAIL_HOST_PASSWORD = config["EMAIL_PASSWORD"]
+EMAIL_HOST_USER = os.getenv("EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
