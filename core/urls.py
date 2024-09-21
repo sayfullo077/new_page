997ac73931e68f11b01191a4441d673d13eb0c22
sayfullo077 <new81305@gmail.com>
2024-09-21 20:13:27 +0500

    fixed

--- core/urls.py
+++ core/urls.py
@@ -30,6 +30,5 @@ if settings.DEBUG:
         path("__debug__/", include("debug_toolbar.urls")),
     ]
 
-# Statik va media fayllarini qo'shamiz
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
